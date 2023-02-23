f5cac298c05b3ec470a6828b545c599160e5a689
DarkShark02 <flavio.ruggiero27@gmail.com>
2023-02-23 14:48:30 +0100

    commenti

--- src/GreenPassUtility.c
+++ src/GreenPassUtility.c
@@ -1,21 +1,20 @@
 #include "GreenPassUtility.h"
 
-// Controllo del formato e lunghezza del codice della tessera Sanitaria
+// Controlliamo il formato e lunghezza del codice della tessera Sanitaria
 void checkHealtCardNumber (char * codiceTesseraSanitaria) {
     size_t codiceTesseraSanitariaLength = strlen(codiceTesseraSanitaria);
     if (codiceTesseraSanitariaLength + 1 != LUNGHEZZA_CODICE_TESSERA_SANITARIA) raiseError(CHECK_HEALTH_CARD_NUMBER_SCOPE, CHECK_HEALTH_CARD_NUMBER_ERROR);
 }
 
 
-// Procedura per acquisire la configurazione riportata nel file di configurazione passato come parametro.
+// Questa procedura si occupa di acquisire la configurazione riportata nel file di configurazione passato come parametro.
 void retrieveConfigurationData (const char * percorsoFileConfigurazione, char ** configurationIP, unsigned short int * configurationPort) {
     FILE * filePointer;
     size_t IPlength = 0, portLength = 0;
     ssize_t getLineBytes;
     char * tempStringConfigurationIP = NULL, * stringServerAddressPort = NULL;
-    //apertura del file di configurazione passato come parametro
+    //--Apriamo il file di configurazione passato come parametro
     filePointer = fopen(percorsoFileConfigurazione, "r");
-    //se l'apertura del file fallisce lanciamo un errore
     if (!filePointer) raiseError(FOPEN_SCOPE, FOPEN_ERROR);
     if ((getLineBytes = getline((char ** restrict) & tempStringConfigurationIP, (size_t * restrict) & IPlength, (FILE * restrict) filePointer)) == -1) raiseError(GETLINE_SCOPE, GETLINE_ERROR);
     * configurationIP = (char *) calloc(strlen(tempStringConfigurationIP), sizeof(char));
@@ -25,14 +24,14 @@ void retrieveConfigurationData (const char * percorsoFileConfigurazione, char **
     if ((getLineBytes = getline((char ** restrict) & stringServerAddressPort, (size_t * restrict) & portLength, (FILE * restrict) filePointer)) == -1) raiseError(GETLINE_SCOPE, GETLINE_ERROR);
     * configurationPort = (unsigned short int) strtoul((const char * restrict) stringServerAddressPort, (char ** restrict) NULL, 10);
     if (configurationPort == 0 && (errno == EINVAL || errno == ERANGE)) raiseError(STRTOUL_SCOPE, STRTOUL_ERROR);
-    //deallochiamo i puntatori
+    //--Deallochiamo i puntatori
     free(tempStringConfigurationIP);
     free(stringServerAddressPort);
-    //chiusura del file
+    //--Chiudiamo del file
     fclose(filePointer);
 }
 
-// Calcolo del periodo di validità di un nuovo Vanilla Green Pass
+// Calcoliamo il periodo di validità di un nuovo Vanilla Green Pass
 char * getVaccineExpirationDate (void) {
     time_t systemTimeSeconds = time(NULL);
     // Si considera la data di sistema attuale
@@ -49,7 +48,7 @@ char * getVaccineExpirationDate (void) {
         expirationDateTimeInfo->tm_mon = (expirationDateTimeInfo->tm_mon + MESI_ATTESA_PROSSIMA_SOMMINISTRAZIONE + 1);
     }
     
-    // Conversione in stringa della data e ritorno al chiamante.
+    //--Convertiamo in stringa della data e ritorno al chiamante.
     char * vaccineExpirationDate = (char *) calloc(LUNGHEZZA_DATA, sizeof(char));
     if (!vaccineExpirationDate) raiseError(CALLOC_SCOPE, CALLOC_ERROR);
     sprintf(vaccineExpirationDate, "%02d-%02d-%d", expirationDateTimeInfo->tm_mday, expirationDateTimeInfo->tm_mon, expirationDateTimeInfo->tm_year + 1900);
@@ -57,7 +56,7 @@ char * getVaccineExpirationDate (void) {
     return vaccineExpirationDate;
 }
 
-// Procedura per ottenere la data attuale
+// Questa procedura si occupa di calcolare e restutire la data attuale
 char * getNowDate (void) {
     time_t tempTimeSeconds = time(NULL);
     struct tm * nowDateTimeInfo = localtime((const time_t *) & tempTimeSeconds);
@@ -70,9 +69,8 @@ char * getNowDate (void) {
 }
 
 /*
-La seguente funzione ritorna il socket file descriptor di una connessione impostata con il ServerV.
-Si passa in input il file di configurazione che è stati fornito al CentroVaccinale o al ServerG per mettersi
-in contatto con il ServerV.
+ --Attraverso la seguente funzione possiamo ritornare il socket file descriptor di una connessione impostata col serverV.
+In input passiamo il file di configurazione che è stati fornito al CentroVaccinale o al ServerG per mettersi in contatto con il ServerV.
 */
 int createConnectionWithServerV (const char * percorsoFileConfigurazione) {
     struct sockaddr_in serverV_Address;
@@ -82,7 +80,7 @@ int createConnectionWithServerV (const char * percorsoFileConfigurazione) {
     
     // Effettivo "retrieve" del file di configurazione
     retrieveConfigurationData(percorsoFileConfigurazione, & stringServerV_AddressIP, & serverV_Port);
-    // Si imposta la comunicazione col serverV
+    //--Impostiamo la comunicazione col serverV
     serverV_SFD = wsocket(AF_INET, SOCK_STREAM, 0);
     memset((void *) & serverV_Address, 0, sizeof(serverV_Address));
     serverV_Address.sin_family = AF_INET;
